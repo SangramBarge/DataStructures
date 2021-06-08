af413de13d31b4a5b4a8e30f69255b5bc2e040ea
Sangram Barge <sangrambarge@Sangrams-MacBook-Air.local>
2021-06-08 14:24:54 +0530

    added logic to delete node with value

--- single_linkedlist.java
+++ single_linkedlist.java
@@ -1,54 +1,63 @@
 
 class Node {
-        int data; 
-        Node next;
-        
-        public Node(int data){
-            this.data = data;
-        }
+    int data; 
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+    }
 }  
 public class Linkedlist {
-    Node head;
-        public void append(int data){
-        if(head ==null){
-            head = new Node(data);
+Node head;
+    public void append(int data){
+    if(head ==null){
+        head = new Node(data);
+        return;
+    }
+    Node current = head;
+    while(current.next!=null){
+        current = current.next;
+    }
+        current.next = new Node(data);
+    }
+
+    public void prepend(int data){
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+    }
+
+    public void printList(){
+     Node current = head;
+        while(current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+    public void deleteWithValue(int data){
+        if(head ==null) return;
+        if(head.data==data){
+            head = head.next;
             return;
+           
         }
         Node current = head;
         while(current.next!=null){
-            current = current.next;
-        }
-            current.next = new Node(data);
-        }
-    
-        public void prepend(int data){
-            Node newHead = new Node(data);
-            newHead.next = head;
-            head = newHead;
-        }
-    
-        public void printList(){
-         Node current = head;
-            while(current != null) {
-                System.out.println(current.data);
-                current = current.next;
+            if(current.next.data ==data){
+                current.next = current.next.next;
+                return;
             }
+            current =current.next;
         }
-    
-    public static void main(String[] args) {
-        Linkedlist list = new Linkedlist();
-        list.append(1);
-        list.append(2);
-        list.printList();
-        list.prepend(0);
-        list.printList();
-        
     }
+
+public static void main(String[] args) {
+    Linkedlist list = new Linkedlist();
+    list.append(1);
+    list.prepend(2);
+    list.printList();
+    list.deleteWithValue(1);
+    list.printList();
+    
 }
-
-
-
-
-
-
-
+}
